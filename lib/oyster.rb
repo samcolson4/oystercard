d6f578988da5acc860fe84af92c5a756e77adb7f
Victoria Jusko <vikjusko@gmail.com>
2020-09-07 15:35:33 +0100

    added balance top up and balance max

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,10 +1,21 @@
 class Oystercard
   DEAFULT_BALANCE = 0.00
+  MAX_BALANCE = 90.00
 
-  attr_accessor :balance
+  attr_reader :balance, :exceeded_balance 
 
   def initialize(balance = DEAFULT_BALANCE)
     @balance = balance
   end
+
+  def top_up(amount)
+    raise "You've exceeded the amount" if exceeded_balance?(amount)
+    @balance += amount 
+   return "Your card is topped up"
+  end 
+
+def exceeded_balance?(amount)
+  (@balance += amount) > MAX_BALANCE
+end 
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,4 +5,23 @@ describe Oystercard do
      # set the balance
       it { expect(subject.balance).to be_an Float } 
   end
+
+  describe "topping up the card" do 
+    it "tops up the card by a given amount" do 
+      amount = 0.01 
+     expect(subject.top_up(amount)).to eq "Your card is topped up"
+  end 
+
+  context "balance is exceeded"  do 
+    before do
+      allow(subject).to receive(:exceeded_balance?).and_return true
+    end
+    it "topping up and exceeding balance and giving error message" do 
+      amount = 95.00
+      expect { subject.top_up(amount) }.to raise_error "You've exceeded the amount"
+    end 
+  end 
+end 
+
+
 end
